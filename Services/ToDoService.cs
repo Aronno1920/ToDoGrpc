bdca928cacfa7f3e3880f1a301fefc7882f8c02b
Selim Ahmed <aronno1920@hotmail.com>
2024-11-04 12:51:22 +0600

    Update full action

--- Services/ToDoService.cs
+++ Services/ToDoService.cs
@@ -1,4 +1,5 @@
 using Grpc.Core;
+using Microsoft.EntityFrameworkCore;
 using ToDoGrpc.Data;
 using ToDoGrpc.Models;
 
@@ -31,31 +32,78 @@ public override async Task<CreateToDoResponse> CreateToDo (CreateToDoRequest req
     });
 }
 
+public override async Task<ReadToDoResponse> ReadToDo (ReadToDoRequest request, ServerCallContext context){
+    if(String.IsNullOrWhiteSpace(request.Id.ToString())){
+        throw new RpcException(new Status(StatusCode.InvalidArgument,"Must provide Todo Id"));
+    }
 
+    var todoItem = await _dbContext.toDoItems.FirstOrDefaultAsync(t => t.Id == request.Id);
 
+    if(todoItem != null){
+        return await Task.FromResult(new ReadToDoResponse{
+            Id = todoItem.Id,
+            Title = todoItem.Title,
+            Description = todoItem.Description,
+            Status = todoItem.Status
+        });
+    }
 
-//     rpc CreateToDo(CreateToDoRequest) returns (CreateToDoResponse)
-// {
+    throw new RpcException(new Status(StatusCode.NotFound,"No found with the Id"));
+}
 
-// }
+public override async Task<GetAllResponse> ReadAllToDo (GetAllRequest request, ServerCallContext context){
+    var response = new GetAllResponse();
+    var todoItems = await _dbContext.toDoItems.ToListAsync();
 
-// // Read Single
-// rpc ReadToDo(ReadToDoRequest) returns (ReadToDoResponse){
+    foreach(var item in todoItems){
+        response.ToDo.Add(new ReadToDoResponse{
+            Id = item.Id,
+            Title = item.Title,
+            Description = item.Description,
+            Status = item.Status
+        });
+    }
 
-// }
+    return await Task.FromResult(response);
+}
 
-// // Read List
-// rpc ReadAllToDo(GetAllRequest) returns (GetAllResponse){
-    
-// }
+public override async Task<UpdateToDoResponse> UpdateToDo (UpdateToDoRequest request, ServerCallContext context){
+    if(String.IsNullOrWhiteSpace(request.Id.ToString())){
+        throw new RpcException(new Status(StatusCode.InvalidArgument,"Must provide Todo Id"));
+    }
 
-// // Update
-// rpc UpdateToDo(UpdateToDoRequest) returns (UpdateToDoResponse){
+    var existingItem = await _dbContext.toDoItems.FirstOrDefaultAsync(t => t.Id==request.Id);
+    if(existingItem == null){
+        throw new RpcException(new Status(StatusCode.NotFound,"No Todo found with the Id"));
+    }
 
-// }
+    existingItem.Title = request.Title;
+    existingItem.Description=request.Description;
+    existingItem.Status=request.Status;
 
-// // Delete
-// rpc DeleteToDo(DeleteToDoRequest) returns (DeleteToDoResponse){
-    
-// }
+    await _dbContext.SaveChangesAsync();
+
+    return await Task.FromResult(new UpdateToDoResponse{
+        Id= existingItem.Id
+    });
+}
+
+public override async Task<DeleteToDoResponse> DeleteToDo (DeleteToDoRequest request, ServerCallContext context){
+    if(String.IsNullOrWhiteSpace(request.Id.ToString())){
+        throw new RpcException(new Status(StatusCode.InvalidArgument,"Must provide Todo Id"));
+    }
+
+    var existingItem = await _dbContext.toDoItems.FirstOrDefaultAsync(t => t.Id==request.Id);
+    if(existingItem == null){
+        throw new RpcException(new Status(StatusCode.NotFound,"No Todo found with the Id"));
+    }
+
+    _dbContext.Remove(existingItem);
+    await _dbContext.SaveChangesAsync();
+
+    return await Task.FromResult(new DeleteToDoResponse{
+        Id= existingItem.Id
+    });
+}
+
 }
